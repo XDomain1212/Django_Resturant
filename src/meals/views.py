6b1eef3b51e0641a1d8d73f10e8f02dbeb3b96e0
XDomain1212 <19914014@qq.com>
2020-07-08 18:12:03 +0800

    uploadCode

--- src/meals/views.py
+++ src/meals/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Meals
 # Create your views here.
+def meal_list(request):
+    meal_list=Meals.objects.all()
+    context={'meal_lsit':meal_list,}
+    return render(request,'meal/list.html',context)
+
+def meal_detail(request,slug):
+    pass
